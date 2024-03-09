12ea096007db12906838e2fa5af547cfd593b67c
NikKowPHP <nik.kow@outlook.com>
2024-03-09 11:33:13 +0100

    feat: Add motion effects to the home section

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,6 +7,7 @@ import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
 
 type Props = {
 	setSelectedPage: (selectedPage: SelectedPage) => void;
@@ -15,16 +16,23 @@ type Props = {
 export function Home({ setSelectedPage }: Props) {
 	const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 	return (
-		<section
-			id="home"
-			className="gap-16 bg-gray pb-10 py-10 md:h-full md:pb-0"
-		>
+		<section id="home" className="gap-16 bg-gray pb-10 py-10 md:h-full md:pb-0">
 			{/* IMAGE AND MAIN HEADER */}
 			<div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
 				{/* MAIN HEADER */}
 				<div className="z-10 mt-32 md:basis-3/5">
 					{/* HEADINGS */}
-					<div className="md:-mt-20">
+					<motion.div
+						className="md:-mt-20"
+						initial="hidden"
+						whileInView="visible"
+						viewport={{ once: true, amount: 0.5 }}
+						transition={{ duration: 0.5 }}
+						variants={{
+							hidden: { opacity: 0, x: -50 },
+							visible: { opacity: 1, x: 0 },
+						}}
+					>
 						<div className="relative">
 							<div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
 								<img src={HomePageText} alt="home-page-text" />
@@ -36,9 +44,19 @@ export function Home({ setSelectedPage }: Props) {
 							officia pariatur sint soluta consectetur perferendis, architecto
 							eos neque distinctio, odio officiis corrupti quis.
 						</p>
-					</div>
+					</motion.div>
 					{/* ACTIONS */}
-					<div className="mt-8 flex items-center gap-8">
+					<motion.div
+						initial="hidden"
+						whileInView="visible"
+						viewport={{ once: true, amount: 0.5 }}
+						transition={{ delay: 0.2, duration: 0.5 }}
+						variants={{
+							hidden: { opacity: 0, x: -50 },
+							visible: { opacity: 1, x: 0 },
+						}}
+						className="mt-8 flex items-center gap-8"
+					>
 						<ActionButton setSelectedPage={setSelectedPage}>
 							Join Now
 						</ActionButton>
@@ -49,27 +67,27 @@ export function Home({ setSelectedPage }: Props) {
 						>
 							<p>Learn More</p>
 						</AnchorLink>
-					</div>
+					</motion.div>
 				</div>
 
 				{/* IMAGE */}
 				<div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
-          <img src={HomePageGraphic} alt="homoe-pageGraphic" />
-        </div>
+					<img src={HomePageGraphic} alt="homoe-pageGraphic" />
+				</div>
 			</div>
 
-      {/* SPONSORS */} 
-      {isAboveMediumScreens && (
-        <div className="h-[150px] w-full bg-primary-100 py-10">
-          <div className="mx-auto w-5/6">
-            <div className="flex w-3/5 items-center justify-between">
-              <img src={SponsorRedBull} alt="redbull-sponsor" />
-              <img src={SponsorForbes} alt="forbes-sponsor" />
-              <img src={SponsorFortune} alt="fortune-sponsor" />
-            </div>
-          </div>
-        </div>
-      )}
+			{/* SPONSORS */}
+			{isAboveMediumScreens && (
+				<div className="h-[150px] w-full bg-primary-100 py-10">
+					<div className="mx-auto w-5/6">
+						<div className="flex w-3/5 items-center justify-between">
+							<img src={SponsorRedBull} alt="redbull-sponsor" />
+							<img src={SponsorForbes} alt="forbes-sponsor" />
+							<img src={SponsorFortune} alt="fortune-sponsor" />
+						</div>
+					</div>
+				</div>
+			)}
 		</section>
 	);
 }

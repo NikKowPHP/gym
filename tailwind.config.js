f8978117c4b0a35d3d410486fb1b4cc21e941993
NikKowPHP <nik.kow@outlook.com>
2024-03-09 08:20:51 +0100

    feat: Define tailwind custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gray-20": "#F8F4EB",
+        "gray-50": "#EFE6E6",
+        "gray-100": "#DFCCCC",
+        "gray-500": "#5E0000",
+        "primary-100": "#ffA6A3"
+      },
+      backgroundImage: (theme) => ({
+        "gradient-yellowred": "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
+        "mobile-home": "url('./assets/HomePageGraphic.png)"
+      }), 
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"]
+      },
+      screens: {
+        xs: "480px",
+        sm: "768px",
+        md: "1060px",
+      },
+    },
   },
   plugins: [],
 }
-

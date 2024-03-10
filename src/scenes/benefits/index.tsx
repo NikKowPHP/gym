f36b91362d4eb907bc0b53b0885a31d997bd8d64
NikKowPHP <nik.kow@outlook.com>
2024-03-10 09:44:51 +0100

    feat: Finish benefits page, add motions

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -9,7 +9,8 @@ import {
 	AcademicCapIcon,
 } from "@heroicons/react/24/solid";
 import { Htext } from "@/shared/Htext";
-import { Benefit } from "./Benefit";
+import { Benefit } from "@/scenes/benefits/Benefit";
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
 
 const benefits: Array<BenefitType> = [
 	{
@@ -50,7 +51,17 @@ export function Benefits({ setSelectedPage }: Props) {
 				onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
 			>
 				{/* HEADER */}
-				<div className="md:my-5 md:w-3/5">
+				<motion.div
+					initial="hidden"
+					whileInView="visible"
+					viewport={{ once: true, amount: 0.5 }}
+					transition={{ duration: 0.5 }}
+					variants={{
+						hidden: { opacity: 0, x: -50 },
+						visible: { opacity: 1, x: 0 },
+					}}
+					className="md:my-5 md:w-3/5"
+				>
 					<Htext>MORE THAT JUST GYM.</Htext>
 					<p className="my-5 text-sm">
 						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio aut
@@ -58,14 +69,14 @@ export function Benefits({ setSelectedPage }: Props) {
 						rem commodi repudiandae, ratione accusantium nihil! Ad et nam minima
 						quia.
 					</p>
-				</div>
+				</motion.div>
 				{/* BENEFITS */}
-				<motion.div className=" items-center justify-between gap-8 mt-5 md:flex"
-				initial="hidden"
-				whileInView="visible"
-				viewport={{once: true, amount: 0.5}}
-				variants={container}
-				
+				<motion.div
+					className=" items-center justify-between gap-8 mt-5 md:flex"
+					initial="hidden"
+					whileInView="visible"
+					viewport={{ once: true, amount: 0.5 }}
+					variants={container}
 				>
 					{benefits.map((benefit: BenefitType) => (
 						<Benefit
@@ -77,6 +88,72 @@ export function Benefits({ setSelectedPage }: Props) {
 						/>
 					))}
 				</motion.div>
+				{/* GRAPHICS AND DESCRIPTION */}
+				<div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+					{/* GRAPHIC */}
+					<img
+						src={BenefitsPageGraphic}
+						alt="benefits-page-graphic"
+						className="mx-auto "
+					/>
+					{/* DESCRIPTION*/}
+					<div>
+						{/* TITLE */}
+						<div className="relative">
+							<div className="before:absolute before:-top-20 before:-left-20 before:z-[1] before:content-abstractwaves">
+								<motion.div
+									initial="hidden"
+									whileInView="visible"
+									viewport={{ once: true, amount: 0.5 }}
+									transition={{ duration: 0.5 }}
+									variants={{
+										hidden: { opacity: 0, x: 50 },
+										visible: { opacity: 1, x: 0 },
+									}}
+								>
+									<Htext>
+										MILLIONS OF HAPPY MEMBERS GETTING{" "}
+										<span className="text-primary-500">FIT</span>.
+									</Htext>
+								</motion.div>
+							</div>
+						</div>
+
+						{/* DESCRIPTION */}
+						<motion.div
+							initial="hidden"
+							whileInView="visible"
+							viewport={{ once: true, amount: 0.5 }}
+							transition={{ delay: 0.2, duration: 0.5 }}
+							variants={{
+								hidden: { opacity: 0, x: 50 },
+								visible: { opacity: 1, x: 0 },
+							}}
+						>
+							<p className="my-5">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Deleniti mollitia quas recusandae sequi corporis quo laborum a
+								saepe earum reprehenderit praesentium consequuntur quisquam,
+								nobis quae tenetur iusto? Est, nihil sint.
+							</p>
+							<p className="mb-5">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Deleniti mollitia quas recusandae sequi corporis quo laborum a
+								saepe earum reprehenderit praesentium consequuntur quisquam,
+								nobis quae tenetur iusto? Est, nihil sint.
+							</p>
+						</motion.div>
+
+						{/* BUTTON */}
+						<div className="relative mt-16">
+							<div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+								<ActionButton setSelectedPage={setSelectedPage}>
+									Join Now
+								</ActionButton>
+							</div>
+						</div>
+					</div>
+				</div>
 			</motion.div>
 		</section>
 	);

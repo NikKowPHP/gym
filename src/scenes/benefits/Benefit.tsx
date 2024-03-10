52575b98b56f1b3cb0f8a3213459d980c61e5a8c
NikKowPHP <nik.kow@outlook.com>
2024-03-10 09:07:49 +0100

    add motion effect of stagged children

--- src/scenes/benefits/Benefit.tsx
+++ src/scenes/benefits/Benefit.tsx
@@ -1,4 +1,5 @@
 import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
@@ -7,9 +8,17 @@ type Props = {
 	description: string;
 	setSelectedPage: (value: SelectedPage) => void;
 };
+const childVariant = {
+  hidden: {opacity: 0, scale: 0.9},
+  visible: {opacity: 1, scale: 1 }
+}
 export function Benefit({ icon, title, description, setSelectedPage }: Props) {
 	return (
-		<div className="mt-5 rounded md border-2 border-gray-100 px-5 py-16 text-center">
+		<motion.div className="mt-5 rounded md border-2 border-gray-100 px-5 py-16 text-center"
+    variants={childVariant}
+    
+    >
+
 			<div className="mb-4 flex justify-center">
 				<div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
 					{icon}
@@ -24,6 +33,6 @@ export function Benefit({ icon, title, description, setSelectedPage }: Props) {
 			>
 				<p>Learn More</p>
 			</AnchorLink>
-		</div>
+		</motion.div>
 	);
 }
